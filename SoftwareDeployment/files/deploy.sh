520185ca77468ce8381b43e0b989700954c487b1
Rob Coward <rob@jive-videos.net>
2016-12-07 12:43:35 +0000

    Changes to try getting output from the deployed script

--- SoftwareDeployment/files/deploy.sh
+++ SoftwareDeployment/files/deploy.sh
@@ -2,3 +2,9 @@
 #
 
 echo "Hello $firstname $lastname"
+
+echo "Writing to /tmp/demo"
+echo "Hello $firstname $lastname" > /tmp/demo
+echo -n "The file /tmp/demo contains `cat /tmp/demo` for server $deploy_server_id during $deploy_action" > $heat_outputs_path.result
+echo "Written to /tmp/demo"
+echo "Output to stderr" 1>&2
